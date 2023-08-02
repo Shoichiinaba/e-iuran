b0c4acc0b8434cf9cf6f334111f85a9bbd2447e4
oktadha01 <oktadha01@gmail.com>
2023-08-02 14:40:28 +0700

    dashboard-client

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,7 +15,61 @@ class Auth extends CI_Controller
 		$this->load->model('M_auth');
 	}
 
+	// function login Warga
 	public function index()
+	{
+		$session = $this->session->userdata('status');
+
+		if ($session == '') {
+			$this->load->view('warga/login');
+		} else {
+			redirect('Dashboard');
+			// $role = $this->session->userdata('status')->role;
+			// if ($role == 'Warga') {
+
+			// } else {
+			// 	redirect('Client_dash');
+			// }
+		}
+	}
+
+	public function client_login()
+	{
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[2]|max_length[15]');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+
+		if ($this->form_validation->run() == TRUE) {
+			$username = trim($_POST['username']);
+			$password = trim($_POST['password']);
+
+			$data = $this->M_auth->m_client_login($username, $password);
+
+			if ($data == false) {
+				$this->session->set_flashdata('result_login', '<br>Email atau Password yang anda masukkan salah.');
+				redirect('Auth');
+			} else {
+				$session = [
+					'userdata' => $data,
+					'status' => "Loged in"
+				];
+				$this->session->set_userdata($session);
+				redirect('Client_dash');
+			}
+		} else {
+			$this->session->set_flashdata('result_login', '<br>email Dan Password Harus Diisi.');
+			redirect('Auth');
+		}
+	}
+
+	function logout()
+	{
+		$this->session->sess_destroy();
+		$this->session->set_flashdata('sukses', 'Anda Telah Keluar dari Aplikasi');
+		redirect('Auth');
+	}
+
+	// function login Admin
+	public function admin()
 	{
 		$session = $this->session->userdata('status');
 
@@ -52,12 +106,5 @@ class Auth extends CI_Controller
 			$this->session->set_flashdata('result_login', '<br>email Dan Password Harus Diisi.');
 			redirect('Auth');
 		}
-	}
-
-	function logout()
-	{
-		$this->session->sess_destroy();
-		$this->session->set_flashdata('sukses', 'Anda Telah Keluar dari Aplikasi');
-		redirect('Auth');
 	}
 }
--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -1,13 +1,36 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class M_auth extends CI_Model {
-	public function login($user, $pass) {
+class M_auth extends CI_Model
+{
+
+	public function m_client_login($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->from('warga');
+		$this->db->where('no_rumah', $username);
+		$this->db->where(
+			'password',
+			md5($password)
+		);
+
+		$data = $this->db->get();
+
+		if ($data->num_rows() == 1) {
+			return $data->row();
+		} else {
+			return false;
+		}
+	}
+	public function login($user, $pass)
+	{
 		$this->db->select('*');
 		$this->db->from('admin');
 		$this->db->where('username', $user);
-		$this->db->where('password', md5($pass)
-	);
+		$this->db->where(
+			'password',
+			md5($pass)
+		);
 
 		$data = $this->db->get();
 
