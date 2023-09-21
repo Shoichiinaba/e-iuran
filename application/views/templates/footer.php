f97d264e8e33b8f50204b6c318ee1c8d35416b6e
Shoichiinaba <shoichiinaba01@gmail.com>
2023-09-21 11:04:05 +0700

    update menu tampilan mobile hicare posisi

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer untuk desktop -->
-<div class="d-md-block d-none">
+<div class="d-lg-block d-none">
     <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
