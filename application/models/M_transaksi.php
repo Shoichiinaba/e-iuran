8cee778e276e1fcc7c6a503d9a4b2b32990161f1
Shoichiinaba <shoichiinaba01@gmail.com>
2023-11-03 11:27:18 +0700

    update membembalikan buat tagihan per rt

--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -4,23 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class M_transaksi extends CI_Model
 {
 
-    public function get_warga($id)
+    public function get_warga($id_rtrw)
     {
         // untuk data per rt
-        // $this->db->select('warga.id_warga, warga.nama, warga.id_rtrw, warga.no_rumah, perumahan.id_perumahan, perumahan.nama as nama_perum');
-        // $this->db->from('warga');
-        // $this->db->join('perumahan', 'warga.id_perum = perumahan.id_perumahan');
-        // $this->db->where('id_rtrw', $id);
-        // $query = $this->db->get();
-        // return $query->result();
-
-        // untuk data warga per perumahan
         $this->db->select('warga.id_warga, warga.nama, warga.id_rtrw, warga.no_rumah, perumahan.id_perumahan, perumahan.nama as nama_perum');
         $this->db->from('warga');
         $this->db->join('perumahan', 'warga.id_perum = perumahan.id_perumahan');
-        $this->db->where('id_perum', $id);
+        $this->db->where('id_rtrw', $id_rtrw);
         $query = $this->db->get();
         return $query->result();
+
+        // untuk data warga per perumahan
+        // $this->db->select('warga.id_warga, warga.nama, warga.id_rtrw, warga.no_rumah, perumahan.id_perumahan, perumahan.nama as nama_perum');
+        // $this->db->from('warga');
+        // $this->db->join('perumahan', 'warga.id_perum = perumahan.id_perumahan');
+        // $this->db->where('id_perum', $id);
+        // $query = $this->db->get();
+        // return $query->result();
     }
 
     public function get_iuran($id_rtrw)
